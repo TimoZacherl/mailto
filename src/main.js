04af5df4e9dc4c58e4beeb25986800ea45664508
Timo <38291523+TimoZacherl@users.noreply.github.com>
2020-09-28 22:34:47 +0200

    ➕ Add fontawesome brands

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import VueClipboard from "vue-clipboard2";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faPaperPlane,
+  faQuestionCircle
+} from "@fortawesome/free-regular-svg-icons";
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-library.add([faPaperPlane]);
+library.add([faPaperPlane, faQuestionCircle, faGithub, faTwitter]);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(VueClipboard);
