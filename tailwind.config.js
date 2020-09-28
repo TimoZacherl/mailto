0e97115517a340d5596da90f8c41fb5bf6b10fee
Timo <38291523+TimoZacherl@users.noreply.github.com>
2020-09-28 22:36:25 +0200

    💄 Add support for dark-mode to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   purge: false,
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        "dark-mode": { raw: "(prefers-color-scheme: dark)" }
+      }
+    }
   },
   variants: {},
   plugins: []
