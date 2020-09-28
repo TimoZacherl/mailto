0b87df8fd14d753543e4f0cda30ce2001fbc7b3b
Timo <38291523+TimoZacherl@users.noreply.github.com>
2020-09-28 22:43:14 +0200

    🐛 Fix 404s on deployment

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+/*
+ * File: vue.config.js
+ * Project: timo
+ * File Created: Monday, 21st September 2020 8:12:24 pm
+ * Author: Timo Zacherl
+ * License: MPL-2.0
+ *
+ * -----
+ * Last Modified: Monday, 21st September 2020 8:27:05 pm
+ * Modified By: Timo Zacherl
+ * -----
+ *
+ * Copyright © 2020 Timo Zacherl. All rights reserved.
+ */
+
+module.exports = {
+  publicPath: "/mailto/"
+};
