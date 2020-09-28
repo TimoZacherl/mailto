a1c5836efac76fd676ab8d9b15eeb043f6e8b081
Timo <38291523+TimoZacherl@users.noreply.github.com>
2020-09-28 15:33:32 +0200

    ➕ Add fontawesome for vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import VueClipboard from "vue-clipboard2";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add([faPaperPlane]);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(VueClipboard);
 
